9231d1144f636e1aa5fe1e1231284e4c849453da
emashkin <e.n.mashkin@gmail.com>
2016-07-21 01:20:50 +0300

    Added cofiguration port, pass and path. Debug garbage removed.

--- Resources/scripts/runPloneDeploy.sh
+++ Resources/scripts/runPloneDeploy.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
-#  Licensed under the Apache License, Version 2.0 (the "License"); you may
-#  not use this file except in compliance with the License. You may obtain
-#  a copy of the License at
 #
-#       http://www.apache.org/licenses/LICENSE-2.0
+#  Plone uses GPL version 2 as its license. As of summer 2009, there are
+#  no active plans to upgrade to GPL version 3.
+#  You may obtain a copy of the License at
 #
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#  License for the specific language governing permissions and limitations
-#  under the License.
+#       http://www.gnu.org
+#
 
 PL_PATH="$1"
 PL_PASS="$2"
 PL_PORT="$3"
 
-# Redirect stdout & stderr into log file:
-exec &> /tmp/runPloneDeploy.log
-
-echo "Hello world. Starting Pl one installation."
-echo "Current directory - "`cd "$( dirname "$0" )" && pwd`
+# Write log. Redirect stdout & stderr into log file:
+exec &> /var/log/runPloneDeploy.log
 
 # echo "Installing all packages."
 sudo apt-get update
@@ -29,13 +22,12 @@ sudo apt-get -y install python-setuptools python-dev build-essential libssl-dev 
 
 # Install optional system packages for handling of PDF and Office files. Can be omited
 sudo apt-get -y install libreadline-dev wv poppler-utils
-echo "Installing Plone."
 
 # Download the latest Plone unified installer:
 wget --no-check-certificate https://launchpad.net/plone/5.0/5.0.4/+download/Plone-5.0.4-UnifiedInstaller.tgz
 
 # Unzip the latest Plone unified installer:
-tar -xvf Plone-5.0.4-UnifiedInstaller.tgz && echo "Tgz unarchieved."
+tar -xvf Plone-5.0.4-UnifiedInstaller.tgz
 cd Plone-5.0.4-UnifiedInstaller
 
 # Set the port that Plone will listen to on available network interfaces. Editing "http-address" param in buildout.cfg file:
@@ -44,10 +36,8 @@ sed -i "s/^http-address = [0-9]*$/http-address = ${PL_PORT}/" buildout_templates
 # Run the Plone installer in standalone mode
 ./install.sh --password="${PL_PASS}" --target="${PL_PATH}" standalone
 
-echo "Starting up Plone."
 # Start Plone
-echo "${PL_PATH}/zinstance"
 cd "${PL_PATH}/zinstance"
 bin/plonectl start
-echo "Plone started."
+
 
